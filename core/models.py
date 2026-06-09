f7fb829f1aac543be7d00623508f4062efc8c423
astro <germiniasi.federico@gmail.com>
2026-06-09 21:19:10 +0200

    init

---
+++ core/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+
+# estende utente di default di Django con campi personalizzati
+class User(AbstractUser):
+    paese = models.CharField(max_length=100, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+    
+    class Meta:
+        ordering = ['-username']
+
